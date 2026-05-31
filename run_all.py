b931c1d8691bd32aafcfce43a673376bf972ffae
madmadxnis-crypto <madmadxnis@gmail.com>
2026-05-31 22:56:44 +0700

    Update run_all.py

--- run_all.py
+++ run_all.py
@@ -5,7 +5,7 @@ import os
 # os.chdir(r"D:\INSPEKSI MOBIL")
 
 # Daftar script yang mau dijalankan
-scripts = ["scraper.py", "scrape_meratus.py", "scrape_tanto.py", "scrape_icon.py", "scrape_samudera.py", "scrape_temas.py]
+scripts = ["scraper.py", "scrape_meratus.py", "scrape_tanto.py", "scrape_icon.py", "scrape_samudera.py", "scrape_temas.py"]
 
 for script in scripts:
     print(f"🚀 Menjalankan {script}...")
